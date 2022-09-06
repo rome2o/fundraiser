6ecdf14310006ea6ba0ace6f0384f035025d7801
Ali Kazim <ali@devkind.com.au>
2022-09-06 17:45:55 +1000

    Fixing volunteer section

--- components/Volunteers.js
+++ components/Volunteers.js
@@ -6,14 +6,14 @@ export default function Volunteers(){
 
                 <div class="w-full h-full bg-green-500 rounded-2xl md:rounded-3xl relative lg:flex items-center">
                     <div class="lg:block w-full">
-                        <img src="images/hands_3.png" alt="Image" class="relative z-10 " />
+                        <img src="images/hands_3.png" alt="Image" class=" w-full lg:transform-none relative rotate-90 transform z-10" />
                         <img src="images/pattern-2.png" alt="Image" class="absolute top-14 left-40" />
                         <img src="images/pattern.png" alt="Image" class="absolute top-0 z-0" />
                     </div>
 
-                    <div class="lg:relative py-4 lg:py-0 w-full">
+                    <div class="lg:relative py-12 lg:py-0 w-full">
                         <h1 class="font-semibold text-white text-xl md:text-4xl text-center lg:text-left leading-normal md:mb-5 lg:mb-10">For the love of humanity, <br /> help people of Pakistan.</h1>
-                        <div class="flex items-center text-center lg:text-left space-x-5 justify-center md:justify-start mt-4 md:mt-0 ">
+                        <div class="flex items-center text-center lg:justify-start space-x-5 justify-center mt-4 md:mt-0 ">
                             <a 
                             href="https://www.eventbrite.com.au/e/pakistan-floods-fund-raising-fawkner-community-sports-hall-tickets-414411625327"
                             target="_blank"
